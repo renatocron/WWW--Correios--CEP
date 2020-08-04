9191624475b15db95223d6c8a631f825ca2978d9
Renato Cron <renato.cron@gmail.com>
2020-08-03 23:57:00 -0300

    Fix pod, add Test::RequiresInternet

--- lib/WWW/Correios/CEP.pm
+++ lib/WWW/Correios/CEP.pm
@@ -11,14 +11,14 @@ use Encode;
 use utf8;
 
 sub new {
-    my ( $class, $params ) = @_;
+    my ($class, $params) = @_;
 
     my $this = {
         _user_agent => defined $params->{user_agent}
         ? $params->{user_agent}
         : 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
         _lwp_ua      => undef,
-        _lwp_options => $params->{lwp_options} || { timeout => 30 },
+        _lwp_options => $params->{lwp_options} || {timeout => 30},
 
         _post_url => defined $params->{post_url}
         ? $params->{post_url}
@@ -36,48 +36,48 @@ sub new {
 }
 
 sub find {
-    my ( $this, $cep ) = @_;
+    my ($this, $cep, $as_html_tree) = @_;
 
-    my @list_address = $this->_extractAddress($cep);
+    my @list_address = $this->_extractAddress($cep, $as_html_tree);
     $list_address[0]{address_count} = @list_address unless wantarray;
 
     return wantarray ? @list_address : $list_address[0];
 }
 
 sub _extractAddress {
-    my ( $this, $cep ) = @_;
+    my ($this, $cep, $as_html_tree) = @_;
 
     my @result = ();
 
     $cep =~ s/[^\d]//go;
-    $cep = sprintf( '%08d', $cep );
+    $cep = sprintf('%08d', $cep);
 
-    if ( $cep =~ /^00/o || $cep =~ /(\d)\1{7}/ ) {
+    if ($cep =~ /^00/o || $cep =~ /(\d)\1{7}/) {
         $result[0]->{status} = "Error: Invalid CEP number ($cep)";
     }
     else {
-        if ( !defined $this->{_lwp_ua} ) {
+        if (!defined $this->{_lwp_ua}) {
 
-            my $ua = LWP::UserAgent->new( %{ $this->{_lwp_options} } );
-            $ua->agent( $this->{_user_agent} );
-            $ua->timeout( $this->{_lwp_options}{timeout} );
+            my $ua = LWP::UserAgent->new(%{$this->{_lwp_options}});
+            $ua->agent($this->{_user_agent});
+            $ua->timeout($this->{_lwp_options}{timeout});
             $this->{_lwp_ua} = $ua;
         }
         my $ua = $this->{_lwp_ua};
 
-        my $req = HTTP::Request->new( POST => $this->{_post_url} );
+        my $req = HTTP::Request->new(POST => $this->{_post_url});
         $req->content_type('application/x-www-form-urlencoded');
-        $req->content( $this->{_post_content} . $cep );
+        $req->content($this->{_post_content} . $cep);
         eval {
             local $SIG{ALRM} = sub { die "Can't connect to server [alarm timeout]\n" };
-            alarm( $this->{_lwp_options}{timeout} + 1 );
+            alarm($this->{_lwp_options}{timeout} + 1);
 
             # Pass request to the user agent and get a response back
             my $res = $ua->request($req);
 
             # Check the outcome of the response
-            if ( $res->is_success ) {
-                $this->_parseHTML( \@result, $res->content );
+            if ($res->is_success) {
+                $this->_parseHTML(\@result, $res->content, $as_html_tree);
             }
             else {
                 $result[0]->{status} = "Error: " . $res->status_line;
@@ -91,37 +91,43 @@ sub _extractAddress {
 }
 
 sub _parseHTML {
-    my ( $this, $address_ref, $html ) = @_;
+    my ($this, $address_ref, $html, $as_html_tree) = @_;
 
     my $tree = HTML::TreeBuilder::XPath->new;
-    $html = decode( 'iso-8859-1', $html );
+    $html = decode('iso-8859-1', $html);
     $html =~ s/&nbsp;//g;    # <-- findvalue is keeping that
     $tree->parse_content($html);
 
     my $ref = $tree->findnodes('//table[contains(@class,"tmptabela")]/tr[not(th)]');
 
-    while ( my $p = shift(@$ref) ) {
-        my $address = {};
+    while (my $p = shift(@$ref)) {
 
-        $address->{street}       = $p->findvalue('./td[1]');
-        $address->{neighborhood} = $p->findvalue('./td[2]');
-        $address->{cep}          = $p->findvalue('./td[4]');
-
-        ( $address->{location}, $address->{uf} ) =
-          split qr{\s*/\s*} => $p->findvalue('./td[3]');
-
-        if ( $address->{cep} ) {
-            $address->{status} = '';
+        if ($as_html_tree) {
+            push(@$address_ref, $p);
         }
         else {
-            $address->{status} = 'Error: Address not found, something is wrong...';
-        }
+            my $address = {};
 
-        push( @$address_ref, $address );
+            $address->{street}       = $p->findvalue('./td[1]');
+            $address->{neighborhood} = $p->findvalue('./td[2]');
+            $address->{cep}          = $p->findvalue('./td[4]');
+
+            ($address->{location}, $address->{uf})
+              = split qr{\s*/\s*} => $p->findvalue('./td[3]');
+
+            if ($address->{cep}) {
+                $address->{status} = '';
+            }
+            else {
+                $address->{status} = 'Error: Address not found, something is wrong...';
+            }
+
+            push(@$address_ref, $address);
+        }
     }
 
     $address_ref->[0]->{status} = 'Error: Address not found'
-      if ( !@$address_ref );
+      if (!@$address_ref);
 
     return 1;
 }
@@ -139,9 +145,9 @@ WWW::Correios::CEP - Perl extension for extract address from CEP (zip code) numb
 
     use WWW::Correios::CEP;
 
-    my $cepper = WWW::Correios::CEP-new;
+    my $cep = WWW::Correios::CEP->new();
 
-    my $address = $cepper->find( $cep );
+    my $address = $cep->find( $cep );
 
     print $address->{street}; # neighborhood, location, uf
 
@@ -150,6 +156,12 @@ WWW::Correios::CEP - Perl extension for extract address from CEP (zip code) numb
 This module fetches CEP information (Brazilian ZIP codes) directly from
 the Correios website, Brazil's official post office company.
 
+=head2 Good to know
+
+Also, check if the returned CEP matches the informed CEP, some addresses can changed from time to time, for example, 49039-050 is now 49009-010 (today is 2020-07-04).
+Correios still return the address street name, but some providers may not accept it anymore (eg: payment gateways)
+
+Correios API is sometimes unstable, please have a fallback!
 
 =head1 METHODS
 
@@ -181,7 +193,7 @@ Extra options to pass to LWP::UserAgent.
 
 =back
 
-=head2 find( $cep )
+=head2 find( $cep [, $as_html_tree ] )
 
 Recieves the CEP string and tries to get address data. Returns a hashref with the following keys:
 
@@ -202,6 +214,8 @@ Recieves the CEP string and tries to get address data. Returns a hashref with th
 If there is more than one address, it returns a list of hashrefs in list context, or
 just the first hashref in scalar context, together with an "C<address_count>" key with
 the total returned addresses.
+
+If $as_html_tree is passed, the list hahsref is changed to raw object of HTML::TreeBuilder::XPath->findnodes()
 
 =head1 SEE ALSO
 
--- t/WWW-Correios-CEP.t
+++ t/WWW-Correios-CEP.t
@@ -1,21 +1,28 @@
 use utf8;
 
-use Test::More tests => 3;
+use Test::More tests => 4;
 BEGIN { use_ok('WWW::Correios::CEP') }
+use Test::RequiresInternet ('www.buscacep.correios.com.br' => 80);
 
 my $cepper = WWW::Correios::CEP->new;
 
-is( ref $cepper, 'WWW::Correios::CEP', 'WWW::Correios::CEP class ok' );
+is(ref $cepper, 'WWW::Correios::CEP', 'WWW::Correios::CEP class ok');
 diag("downloading...");
 
 my $got  = $cepper->find('03640-000');
 my $expt = {
-    street       => 'Rua Cupá',
-    neighborhood => 'Vila Carlos de Campos',
-    location     => 'São Paulo',
-    uf           => 'SP',
-    cep          => '03640-000',
-    status       => '',
+    street        => 'Rua Cupá',
+    neighborhood  => 'Vila Carlos de Campos',
+    location      => 'São Paulo',
+    uf            => 'SP',
+    cep           => '03640-000',
+    status        => '',
     address_count => 1
 };
-is_deeply( $got, $expt, 'testing address for 03640-000' ) || diag explain $got;
+is_deeply($got, $expt, 'testing address for 03640-000') || diag explain $got;
+
+
+my $got2  = $cepper->find('03640-000', 1);
+is(ref $got2, 'HTML::Element');
+
+
